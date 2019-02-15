2465330adccea1720a61abcfca203ab0311d9e09
laptop <rahman.hasri@gmail.com>
2019-02-15 11:42:58 +0700

    upload file done

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -20,7 +20,7 @@ const sendUploadToGCS = (req, res, next) => {
     return next()
   }
 
-  const gcsname = Date.now() + req.file.originalname
+  const gcsname = Date.now() + req.file.originalname.replace(/\s/g, '')
   const file = bucket.file(gcsname)
 
   const stream = file.createWriteStream({
